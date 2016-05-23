05a8386b91e1c49a01d64b4c85adbb835162cb82
Maxim Eremenko <macbook@MacBook-Pro-MacBook.local>
2016-05-23 13:26:30 +0300

    Initial Commit

---
+++ InstagramMe/Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstagramMe
+//
+//  Created by Maxim Eremenko on 5/23/16.
+//  Copyright © 2016 Maxim Eremenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstagramMe/Supporting Files/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InstagramMe
+//
+//  Created by Maxim Eremenko on 5/23/16.
+//  Copyright © 2016 Maxim Eremenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

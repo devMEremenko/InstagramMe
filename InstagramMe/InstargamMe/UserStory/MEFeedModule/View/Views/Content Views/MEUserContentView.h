7b4fd01e51d4413b0ae5a142089af8cdb90a8564
Maxim Eremenko <macbook@MacBook-Pro-MacBook.local>
2016-05-31 02:31:50 +0300

    like color update

---
+++ InstagramMe/InstargamMe/UserStory/MEFeedModule/View/Views/Content Views/MEUserContentView.h
@@ -0,0 +1,11 @@
+//
+//  MEUserContentView.h
+//  InstagramMe
+//
+//  Created by Maxim Eremenko on 5/31/16.
+//  Copyright © 2016 Maxim Eremenko. All rights reserved.
+//
+
+@interface MEUserContentView : UIView
+
+@end
